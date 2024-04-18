44882aad6370e107516cd327cdda6114859673bb
Gustavo René <renegustavo42@gmail.com>
2024-04-17 23:51:25 -0300

    Criado a model de User

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace checkpointdotnet.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+        public string UserPhone { get; set; } = string.Empty;
+    }
+}
